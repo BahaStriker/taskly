057b469c8bd2ee9006e1e069edea022d4fa3d106
AlaEddinBouker <alaeddin.bouker@gmail.com>
2020-01-20 12:33:03 +0100

    logo and favicon change

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,6 +19,7 @@
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/components.css')}}">
     <link rel="stylesheet" href="{{ asset('assets/css/icons.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css')}}">
     <link href="{{ asset('assets/css/easy-autocomplete.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     @stack('style')
